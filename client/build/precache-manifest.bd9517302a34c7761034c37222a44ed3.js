eb5be6900b52f1d34df43d7a3be638748fbe1b2a
aiotrope <arneli74@protonmail.com>
2023-06-26 18:13:01 +0300

    ♻️ refactor(app): solutions for exercise 13

---
+++ client/build/precache-manifest.bd9517302a34c7761034c37222a44ed3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faef6d30128cfa2024bf4c1fa72381a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5969e0596559235d18d9",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9f80f825b0b5e95755be",
+    "url": "/static/js/2.6f262be4.chunk.js"
+  },
+  {
+    "revision": "5969e0596559235d18d9",
+    "url": "/static/js/main.31800694.chunk.js"
+  },
+  {
+    "revision": "0021d159b8343cfaf18e",
+    "url": "/static/js/runtime~main.d49aaa73.js"
+  }
+]);
